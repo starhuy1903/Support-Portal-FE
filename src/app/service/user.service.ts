0a55f794cec357bc5b1ed4e29c6d65d39d765e1a
huy190302 <huykst137@gmail.com>
2021-10-28 14:57:59 +0700

    Update user service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -19,4 +19,8 @@ export class UserService {
   public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
     return this.http.post<User>(`${this.host}/user/add`, formData);
   }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/update`, formData);
+  }
 }
