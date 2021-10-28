e6811bdbaf9a5457ecd73802b20fb6fe48d28e70
huy190302 <huykst137@gmail.com>
2021-10-28 14:55:13 +0700

    Add user service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -15,4 +15,8 @@ export class UserService {
   public getUsers(): Observable<User[] | HttpErrorResponse> {
     return this.http.get<User[]>(`${this.host}/user/list`);
   }
+
+  public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
 }
