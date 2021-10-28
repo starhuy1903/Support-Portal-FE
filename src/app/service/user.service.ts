c8ab8dc98ee95666fd8a339ed1d0fde257969a74
huy190302 <huykst137@gmail.com>
2021-10-28 15:03:27 +0700

    Reset password service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -23,4 +23,8 @@ export class UserService {
   public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
     return this.http.post<User>(`${this.host}/user/update`, formData);
   }
+
+  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get(`${this.host}/user/resetpassword/${email}`);
+  }
 }
